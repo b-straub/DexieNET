1723e856fad46f507f119787015045d0c40d20ea
Bernhard Straub <bsoft@mandel-straub.de>
2022-12-17 09:12:36 +0100

    Transaction
    Show difference between catched and not catched nested transaction

--- DexieNETTest/TestBase/Test/TestCases/Generell/FailedTransaction.cs
+++ DexieNETTest/TestBase/Test/TestCases/Generell/FailedTransaction.cs
@@ -47,16 +47,39 @@ namespace DexieNETTest.TestBase.Test
             }
 
             var persons = DataGenerator.GetPersons();
-            exThrown = false;
 
+            async Task FailedAdd(IEnumerable<ulong> personKeys, bool ignoreFail)
+            {
+                await DB.Transaction(async _ =>
+                {
+                    var personValues = await DB.Persons().BulkGet(personKeys);
+
+                    if (!ignoreFail)
+                    {
+                        await DB.Persons().Add(personValues.FirstOrDefault());
+                    }
+                    else
+                    {
+                        try
+                        {
+                            await DB.Persons().Add(personValues.FirstOrDefault());
+                        }
+                        catch (Exception ex)
+                        {
+                            exThrown = ex.GetType() != typeof(TransactionException);
+                        }
+                    }               
+                });
+            }
+
+            exThrown = false;
             try
             {
                 await DB.Transaction(async _ =>
                 {
                     await table.Clear();
                     var personKeys = await table.BulkAdd(persons, true);
-                    var personValues = await table.BulkGet(personKeys);
-                    await table.Add(personValues.FirstOrDefault());
+                    await FailedAdd(personKeys, false);
                 });
             }
             catch (Exception ex)
@@ -65,9 +88,26 @@ namespace DexieNETTest.TestBase.Test
             }
 
             count = await table.Count();
+
             if (!exThrown || await table.Count() != 1)
             {
                 throw new InvalidOperationException("Failed transaction executed.");
+            }
+
+            exThrown = false;
+            await DB.Transaction(async _ =>
+            {
+                await table.Clear();
+                var personKeys = await table.BulkAdd(persons, true);
+                await FailedAdd(personKeys, true);
+                await table.Add(DataGenerator.GetPerson3());
+            });
+
+            count = await table.Count();
+
+            if (!exThrown || await table.Count() != 12)
+            {
+                throw new InvalidOperationException("Catched failed transaction not executed.");
             }
 
             return "OK";
