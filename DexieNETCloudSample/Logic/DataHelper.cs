75b13d12d0a2f14e9ac8794fa7b1a739a171e0e7
Bernhard Straub <bsoft@mandel-straub.de>
2023-12-29 18:36:11 +0100

    Cloud Sample
    Add missing files

---
+++ DexieNETCloudSample/Logic/DataHelper.cs
@@ -0,0 +1,43 @@
+﻿using DexieNET;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static DexieNET.UIAlert;
+
+namespace DexieNETCloudSample.Logic
+{
+    public class ToDoListData(string title)
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Title { get; set; } = title;
+
+        [Required]
+        public DateTime DueDate { get; set; }
+    }
+
+    public class ToDoItemData(string text, DateTime date)
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Text { get; set; } = text;
+
+        [Required]
+        public DateTime DueDate { get; set; } = date;
+    }
+
+    public class EmailData(string? placeholder)
+    {
+        [Required(AllowEmptyStrings = false)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+
+        public string Placeholder { get; set; } = placeholder ?? string.Empty;
+    }
+
+    public class OTPData(IEnumerable<UIAlert> alerts)
+    {
+        [StringLength(8, MinimumLength = 8)]
+        [RegularExpression(@"([a-zA-Z0-9]+)", ErrorMessage = "Only letters and numbers allowed")]
+        public string OTP { get; set; } = string.Empty;
+
+        public IEnumerable<UIAlert> Alerts { get; set; } = alerts;
+    }
