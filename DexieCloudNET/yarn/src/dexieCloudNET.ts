1afeb97014c6e2ee5db2635ad0e152ec96578db4
Bernhard Straub <bsoft@mandel-straub.de>
2024-04-26 15:36:57 +0200

    Fix remote build for Webpack bundle

--- DexieCloudNET/yarn/src/dexieCloudNET.ts
+++ DexieCloudNET/yarn/src/dexieCloudNET.ts
@@ -1,9 +1,21 @@
 ﻿import "./dexieCloudNETBase";
 import "./dexieCloudNETCloud";
 import "./dexieCloudNETObservables";
-import "../../../DexieNET/yarn/src/dexieNET"
 
 export * from "./dexieCloudNETBase";
 export * from "./dexieCloudNETCloud";
 export * from "./dexieCloudNETObservables";
-export * from "../../../DexieNET/yarn/src/dexieNET";
+
+import "./DexieNET/dexieNETBase";
+import "./DexieNET/dexieNETLiveQuery";
+import "./DexieNET/dexieNETTable";
+import "./DexieNET/dexieNETTransactions";
+import "./DexieNET/dexieNETCollection";
+import "./DexieNET/dexieNETPersistence";
+
+export * from "./DexieNET/dexieNETBase";
+export * from "./DexieNET/dexieNETLiveQuery";
+export * from "./DexieNET/dexieNETTable";
+export * from "./DexieNET/dexieNETTransactions";
+export * from "./DexieNET/dexieNETCollection";
+export * from "./DexieNET/dexieNETPersistence";
